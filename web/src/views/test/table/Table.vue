e3fe5be346795cd5a4d6c3c34141e9964d841ba6
liudong <liudong@ones.ai>
2022-12-20 21:52:46 +0800

    add: init web

---
+++ web/src/views/test/table/Table.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a-radio-group v-model="size">
+      <a-radio value="small">Small</a-radio>
+      <a-radio value="middle">Middle</a-radio>
+      <a-radio value="large">Large</a-radio>
+    </a-radio-group>
+    <br />
+    <br />
+    <a-space :size="size">
+      <a-button type="primary">Primary</a-button>
+      <a-button>Default</a-button>
+      <a-button type="dashed">Dashed</a-button>
+      <a-button type="link">Link</a-button>
+    </a-space>
+  </div>
+</template>
+  <script>
+  export default {
+    data () {
+      return {
+        size: 'small'
+      }
+    }
+  }
+  </script>
