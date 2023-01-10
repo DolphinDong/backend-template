c04c9f53c3f1b78348772f59b1a514ae3fc77229
liudong <1076624325@qq.com>
2023-01-11 00:28:20 +0800

    Modify: 优化菜单展示功能，新增菜单查询功能

--- web/src/views/system/Menu.vue
+++ web/src/views/system/Menu.vue
@@ -1,50 +1,72 @@
 <!-- eslint-disable vue/no-unused-vars -->
 <template>
-  <div>
-    <a-card :bordered="false">
-      <a-table
-        rowKey="id"
-        :columns="columns"
-        :data-source="data"
-        :expanded-row-keys.sync="expandedRowIds"
-        :loading="loadingTable"
-        size="small"
-        :bordered="false"
-      >
-        <span slot="icon" slot-scope="icon,record">
-          <a-icon :type="icon" /> {{ icon }}
-        </span>
-        <span slot="name" slot-scope="name, record">
-          <a-tag color="green">
-            {{ name }}
-          </a-tag>
-        </span>
-        <span slot="type" slot-scope="type, record">
-          <a-tag v-if="type===1" color="#108ee9">
-            菜单
-          </a-tag>
-          <a-tag v-if="type===2" color="#f50">
-            权限
-          </a-tag>
-        </span>
-        <!-- slot-scope="text, record" -->
-        <span slot="action" >
-          <a v-if="$auth(menuApi + '.put')" >编辑</a>
-          <a-divider type="vertical" />
-          <a-dropdown v-if="$auth(menuApi + '.delete')">
-            <a-menu slot="overlay">
-              <a-menu-item
-                v-if="$auth(menuApi + '.delete')"
-              ><a style="color: red">删除</a></a-menu-item
-              >
-            </a-menu>
-            <a>更多<a-icon type="down" /></a>
-          </a-dropdown>
-        </span>
+  <a-card :bordered="false">
+    <a-form :form="queryParam" :label-col="{ span: 5 }" :wrapper-col="{ span: 17 }">
+      <a-row :gutter="48">
 
-      </a-table>
-    </a-card>
-  </div>
+        <a-col :md="8" :sm="24">
+          <a-form-item label="搜索">
+            <a-input
+              v-model.trim="queryParam.query"
+              @pressEnter="searchMenu"
+              placeholder="请输入关键信息"
+            />
+          </a-form-item>
+        </a-col>
+        <a-col :md="8" :sm="24">
+          <a-button type="primary" @click="searchMenu">查询</a-button>
+          <a-button @click="openClose" style="margin:0px 15px">{{ treeOpen?"折叠":"展开" }}菜单</a-button>
+          <a-button
+            v-if="$auth(menuApi + '.post')"
+            type="primary"
+            icon="plus"
+            @click="addMenu"
+          >添加菜单</a-button>
+        </a-col>
+      </a-row>
+    </a-form>
+    <a-table
+      rowKey="id"
+      :columns="columns"
+      :data-source="data"
+      :expanded-row-keys.sync="expandedRowIds"
+      :loading="loadingTable"
+      size="small"
+      :bordered="false"
+    >
+      <span slot="icon" slot-scope="icon,record">
+        <a-icon v-if="icon" :type="icon" /> {{ icon }}
+      </span>
+      <span slot="name" slot-scope="name, record">
+        <a-tag color="green">
+          {{ name }}
+        </a-tag>
+      </span>
+      <span slot="type" slot-scope="type, record">
+        <a-tag v-if="type===1" color="#108ee9">
+          菜单
+        </a-tag>
+        <a-tag v-if="type===2" color="#f50">
+          权限
+        </a-tag>
+      </span>
+      <!-- slot-scope="text, record" -->
+      <span slot="action" >
+        <a v-if="$auth(menuApi + '.put')" >编辑</a>
+        <a-divider type="vertical" />
+        <a-dropdown v-if="$auth(menuApi + '.delete')">
+          <a-menu slot="overlay">
+            <a-menu-item
+              v-if="$auth(menuApi + '.delete')"
+            ><a style="color: red">删除</a></a-menu-item
+            >
+          </a-menu>
+          <a>更多<a-icon type="down" /></a>
+        </a-dropdown>
+      </span>
+
+    </a-table>
+  </a-card>
 </template>
 
 <script>
@@ -105,9 +127,12 @@ export default {
     return {
         loadingTable: false,
         menuApi: APIS.BaseUrl + APIS.menuApi.menu,
-        data: [],
+        menuTree: [],
         columns,
-        expandedRowIds: [1, 2]
+        data: [],
+        expandedRowIds: [],
+        queryParam: { query: '' },
+        treeOpen: true
     }
   },
   methods: {
@@ -118,7 +143,7 @@ export default {
             const data = res.data
             if (data) {
                 this.changePermissionId(data.menu_tree)
-                this.data = data.menu_tree
+                this.menuTree = data.menu_tree
                 this.expandedRowIds = data.menu_ids
             }
         } catch (e) {
@@ -136,10 +161,76 @@ export default {
           this.changePermissionId(element.children)
         }
       })
+    },
+    async searchMenu (expandAll) {
+        let result = []
+       await this.queryMenus()
+        if (this.queryParam.query) {
+            result = this.findMenu(this.menuTree)
+            this.data = result
+            // 展开所有
+            if (expandAll !== false) {
+              this.expandedRow(this.data)
+            }
+        } else {
+            this.data = this.menuTree
+        }
+    },
+    expandedRow (dataList) {
+      dataList.forEach(element => {
+          this.expandedRowIds.push(element.id)
+          if (element.children && element.children.length > 0) {
+            this.expandedRow(element.children)
+          }
+      })
+    },
+    // 递归查找菜单，如果子菜单符合条件同时把父级菜单给展示出来，如果只有父级菜单符合条件则不展示器子菜单
+    findMenu (dataList) {
+      if (!dataList || dataList.length === 0) {
+        return null
+      }
+        const result = []
+        dataList.forEach(element => {
+            if ((element.name && element.name.indexOf(this.queryParam.query) !== -1) ||
+                (element.path && element.path.indexOf(this.queryParam.query) !== -1) ||
+             (element.component && element.component.indexOf(this.queryParam.query) !== -1) ||
+             (element.redirect && element.redirect.indexOf(this.queryParam.query) !== -1) ||
+             (element.title && element.title.indexOf(this.queryParam.query) !== -1)) {
+                result.push(element)
+                if (element.children && element.children.length > 0) {
+                  const children = this.findMenu(element.children)
+                  // console.log(element.name, '---->', children)
+                  if (children && children.length > 0) {
+                    element.children = children
+                  } else { // 如果这个菜单的没有符合条件的子菜单则清空这个菜单的子菜单
+                    element.children = null
+                  }
+                }
+            } else if (element.children && element.children.length > 0) { // 直接找儿子，如果儿子中符合条件的菜单则把当前这个菜单添加进来
+              const children = this.findMenu(element.children)
+              if (children && children.length > 0) {
+                element.children = children
+                result.push(element)
+                console.log('element', element)
+              }
+            }
+        })
+        return result
+    },
+    addMenu () {
+
+    },
+    openClose () {
+      if (this.treeOpen) {
+        this.expandedRowIds = []
+      } else {
+        this.expandedRow(this.data)
+      }
+      this.treeOpen = !this.treeOpen
     }
   },
   mounted () {
-    this.queryMenus()
+    this.searchMenu(false)
   }
 
 }
